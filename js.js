1e6825a80d9c21ba3a509d55109d89373aff1f43
Samke <samcopansaaa@gmail.com>
2019-10-24 23:12:43 +0200

    Added billing and client side app!

---
+++ js.js
@@ -0,0 +1,16 @@
+// write a function to retrieve a blob of json
+// make an ajax request! Use the 'fetch' function.
+
+// function fetchAlbums() {
+//     fetch('http://rallycoding.herokuapp.com/api/music_albums')
+//         .then(res => res.json())
+//         .then(json => console.log(json));
+// }
+
+const fetchAlbums = async () => {
+    const res = await fetch('http://rallycoding.herokuapp.com/api/music_albums')
+    const json = await res.json()
+    console.log(json);
+}
+
+fetchAlbums();
--- services/passport.js
+++ services/passport.js
@@ -10,10 +10,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById()
-    .then(user => {
+  User.findById(id).then(user => {
       done(null, user);
-    })
+    });
 });
 
 passport.use(
@@ -24,17 +23,15 @@ passport.use(
           callbackURL: '/auth/google/callback',
           proxy: true
         }, 
-        (accessToken, refreshToken, profile, done) => {
-          User.findOne({ googleId: profile.id}).then(existingUser => {
-            if (existingUser) {
-              //we already have that record.
-              done(null, existingUser);
-            } else {
-              new User({ googleId: profile.id })
-                .save()
-                .then(user => done(null, user));
-            }
-          });
+        async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id})
+           
+          if (existingUser) {
+            return done(null, existingUser);
+          } 
+
+          const user = await new User({ googleId: profile.id }).save();
+          done(null, user);
         }
     )
 );
